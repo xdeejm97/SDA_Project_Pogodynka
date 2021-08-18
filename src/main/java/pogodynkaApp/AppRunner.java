e556abea5fe92ff739dca8713775cac36417a27f
Wojtek <zwierzw@wp.pl>
2021-08-18 16:54:13 +0200

    W - adding first console schema

---
+++ src/main/java/pogodynkaApp/AppRunner.java
@@ -0,0 +1,9 @@
+package pogodynkaApp;
+
+public class AppRunner {
+    public static void main(String[] args) {
+        WeatherApp weatherApp = new WeatherApp();
+
+        weatherApp.run();
+    }
+}
